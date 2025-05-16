af01fc6c25249cd41668a8790464d7e4ee1644d8
Yogesh261192 <yogeshmamgain2611@gmail.com>
2025-05-16 12:53:56 +0530

    changes done

---
+++ new_react_training/my-next-app/pages/emailverification.js
@@ -0,0 +1,48 @@
+// EmailVerificationPage.jsx
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { account } from '../appwriteConfig';
+import { useUser } from '../components/userContext';
+
+export default function EmailVerificationPage() {
+  const [message, setMessage] = useState('Verifying...');
+  const navigate = useNavigate();
+  const { setUser } = useUser(); // Or use setIsSignedIn(true)
+//   const account = new Account(client);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const secret = urlParams.get('secret');
+    const userId = urlParams.get('userId');
+
+    if (secret && userId) {
+      account.updateVerification(userId, secret)
+        .then(async () => {
+          const user = await account.get(); // fetch user details
+          setUser(user); // set user context
+          setMessage('✅ Email verified!');
+          setTimeout(() => {
+            navigate('/'); // redirect to homepage
+          }, 1500);
+        })
+        .catch((err) => {
+          console.error('Verification failed:', err);
+          setMessage(`❌ Email verification failed, ${err.message}`);
+          setTimeout(() => {
+            navigate('/'); // redirect to homepage
+          }, 1500);
+        });
+    } else {
+      setMessage('Invalid verification link.');
+      setTimeout(() => {
+        console.log('ii')
+            navigate('/'); // redirect to homepage
+          }, 1500);
+    }
+  }, []);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-xl font-semibold">{message}</h1>
+    </div>
+  );
+}
