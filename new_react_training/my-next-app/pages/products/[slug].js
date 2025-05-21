1e9da401884210b71a92ad5d020083ae5a8afcfe
Yogesh261192 <yogeshmamgain2611@gmail.com>
2025-05-21 15:01:10 +0530

    changes done

--- new_react_training/my-next-app/pages/products/[slug].js
+++ new_react_training/my-next-app/pages/products/[slug].js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { databases, storage } from '../../appwriteConfig';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Footer from '../../components/footer';
 import Image from 'next/image';
 import ShimmerLoader from '../../components/ShimmerProduct';
 import ProductHead from '../../components/ProductHead';
